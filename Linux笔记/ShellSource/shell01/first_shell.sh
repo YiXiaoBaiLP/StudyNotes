885267547a403f158a3903f6d2edc14d218e3c8b
Liu Peng <13891328530@163.com>
2020-03-16 01:55:03 +0800

    Update

---
+++ Linux笔记/ShellSource/shell01/first_shell.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+# 以下是对脚本的说明文件
+#Name ： first_shell.sh
+# Desc:null
+# Path:/shell01/first_shell.sh
+#Usage: /shell01/first_shell.shell01
+#Update: 2020-03-15
+
+
+echo "Helo,Wold!"
+echo 'Helo,Wold!'
+echo "Helo,Wold!"
